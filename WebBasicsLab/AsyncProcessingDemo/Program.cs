4607eb56c08f9d522ee06472276461230aa1c892
Petar Kobakov <pkobakov@gmail.com>
2021-08-25 14:03:37 +0300

    Async Processing

---
+++ WebBasicsLab/AsyncProcessingDemo/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PrimeNumbersCounter
+{
+    class StartUp
+    {
+       static int Count = 0;
+       static object lockObj = new object(); 
+        static void Main(string[] args)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            PrintPrimeCount(1, 1_000_000);
+            Console.WriteLine(sw.Elapsed);
+
+            //race condition:
+           //List<int> numbers = Enumerable.Range(0, 10_000).ToList();
+           
+           //for (int i = 0; i < 4; i++)
+           //{
+           //    new Thread(() => 
+           //    {
+           //        while (numbers.Count>0)
+           //        {
+           //            numbers.RemoveAt(numbers.Count - 1);
+           //        }
+           //    }).Start();
+           //
+           //}
+
+            // Threads:
+
+            // thread initialization:
+
+            //Thread thread2 = new Thread(() => 
+            //{ 
+            //
+            //   code....
+            //
+            //});
+
+           //Thread thread2 = new Thread(()=>PrintPrimeCount(2_500_001, 5_000_000));
+           //Thread thread1 = new Thread(()=>PrintPrimeCount(1,2_500_000));
+           //Thread thread3 = new Thread(()=>PrintPrimeCount(5_000_001, 7_500_000));
+           //Thread thread4 = new Thread(()=>PrintPrimeCount(7_500_001, 10_000_000));
+            
+            
+            //Stopwatch sw = Stopwatch.StartNew();
+            //thread1.Start(); //starts the thread execution
+            //thread2.Start();
+            //thread3.Start();
+            //thread4.Start();
+            
+            //thread1.Join();  //waits the execution of the thread to be finished
+            //thread2.Join();
+            //thread3.Join();
+            //thread4.Join();
+            //Console.WriteLine(Count);
+            //Console.WriteLine(sw.Elapsed);
+            
+            //while (true)
+            //{
+            //    string input = Console.ReadLine();
+            //    Console.WriteLine(input.ToUpper());
+            //}
+
+         // Tasks:
+
+            //Stopwatch sw2 = Stopwatch.StartNew();
+            //List<Task> tasks = new List<Task>();
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    var task = Task.Run(async () => 
+            //    {
+            //        HttpClient httpClient = new HttpClient();
+            //        var url = $"https://vicove.com/vic-{i}";
+            //        var httpResponse = await httpClient.GetAsync(url);
+            //        var vic = await httpResponse.Content.ReadAsStringAsync();
+            //        Console.WriteLine(vic.Length);
+            //    });
+            //
+            //    tasks.Add(task);
+            //    Console.WriteLine(sw2.Elapsed);
+            //}
+
+
+        }
+
+
+
+
+        private static void PrintPrimeCount(int min, int max)
+        {
+
+            //for (int i = min; i <= max; i++)
+            Parallel.For(min, max + 1, i =>
+           {
+               bool isPrime = true;
+
+               for (int j = 2; j <= Math.Sqrt(i); j++)
+               {
+                   if (i % j == 0)
+                   {
+                       isPrime = false;
+                   }
+               }
+
+               if (isPrime)
+               {
+                   lock (lockObj)
+                   {
+                       Count++;
+                   }
+
+               }
+           });
+        }
+    }
+}
---
+++ WebBasicsLab/MyServerDemo/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace HTTPClientDemo
+{
+    public class StartUp
+    {
+        const string Newline = "\r\n";
+        static async Task Main(string[] args)
+        {
+
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 12345);
+            tcpListener.Start();
+
+            while (true)
+            {
+                var client = tcpListener.AcceptTcpClient();
+
+                ProcessClientAsync(client);
+
+            }
+
+        }
+
+        public static async Task ProcessClientAsync(TcpClient client)
+        {
+            using (var stream = client.GetStream())
+            {
+
+
+                byte[] buffer = new byte[1000000];
+                var length = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                string requestString = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+                Console.WriteLine(requestString);
+
+                Thread.Sleep(5000);
+
+                string html = $"<a>Hello from PepiServer {DateTime.Now}</a>" +
+                    $"<form method=post><input name=username /><input name=password />" +
+                    $"<input type=submit /></form>";
+
+                string response = "HTTP/1.1 200 OK" + Newline +
+                    "Server: PepiServer 2021" + Newline +
+                    //"Location: https://www.aninamontanari.com" + Newline +
+                    "Content-Type: text/html; charset= utf-8" + Newline +
+                    "Content-Length:" + html + Newline +
+                    Newline + html + Newline;
+
+                byte[] reponseBytes = Encoding.UTF8.GetBytes(response);
+                await stream.WriteAsync(reponseBytes);
+
+                Console.WriteLine(new string('=', 50));
+
+            }
+        }
+
+        private static async Task ReadData()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string url = "https://softuni.bg/trainings/3164/csharp-web-basics-september-2020";
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            Console.WriteLine("Status code: " + response.StatusCode);
+            Console.WriteLine(string.Join(Environment.NewLine, response.Headers.Select(h => h.Key + ": " + h.Value.FirstOrDefault())));
+        }
+    }
+}
