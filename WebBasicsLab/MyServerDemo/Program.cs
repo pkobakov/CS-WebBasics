c8ce9ca5681af5d8d5977ef2cbeedbb8c1a79b2c
Petar Kobakov <pkobakov@gmail.com>
2021-08-26 18:02:06 +0300

    State Management

--- WebBasicsLab/MyServerDemo/Program.cs
+++ WebBasicsLab/MyServerDemo/Program.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Text;
 using System.Threading;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace HTTPClientDemo
 {
     public class StartUp
     {
         const string Newline = "\r\n";
+        static Dictionary<string, int> SessionStorage = new Dictionary<string, int>();
         static async Task Main(string[] args)
         {
 
@@ -22,7 +25,7 @@ namespace HTTPClientDemo
             {
                 var client = tcpListener.AcceptTcpClient();
 
-                ProcessClientAsync(client);
+                await ProcessClientAsync(client);
 
             }
 
@@ -37,19 +40,38 @@ namespace HTTPClientDemo
                 byte[] buffer = new byte[1000000];
                 var length = await stream.ReadAsync(buffer, 0, buffer.Length);
 
-                string requestString = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+                string requestString = Encoding.UTF8.GetString(buffer, 0, length);
                 Console.WriteLine(requestString);
 
+                var sid = Guid.NewGuid().ToString();
+                var match = Regex.Match(requestString, @"sid=[^\n]*\r\n");
+               
 
-                string html = $"<a>Hello from PepiServer {DateTime.Now}</a>" +
+                if (match.Success)
+                {
+                    sid = match.Value.Substring(4);
+                }
+
+
+
+                Console.WriteLine(sid);
+
+                if (!SessionStorage.ContainsKey(sid))
+                {
+                    SessionStorage.Add(sid, 0);
+                }
+
+                SessionStorage[sid]++;
+
+                string html = $"<a>Hello from PepiServer {DateTime.UtcNow}</a>" +
+                    $"<a>Number of visits: {SessionStorage[sid]}</a>" +
                     $"<form method=post><input name=username /><input name=password />" +
                     $"<input type=submit /></form>";
 
                 string response = "HTTP/1.1 200 OK" + Newline +
                     "Server: PepiServer 2021" + Newline +
-                    //"Location: https://www.aninamontanari.com" + Newline +
-                    "Set-Cookie: sid=myFirstCookie" + Newline +
-                     "Set-Cookie: sid2=mySecondCookie" + Newline +
+                   //"Location: https://www.aninamontanari.com" + Newline +
+                   $"Set-Cookie: sid={sid}; Path= /; HttpOnly; Expires=" + DateTime.UtcNow.AddHours(3).ToString("R") + Newline +
                     "Content-Type: text/html; charset= utf-8" + Newline +
                     "Content-Length:" + html + Newline +
                     Newline + html + Newline;
