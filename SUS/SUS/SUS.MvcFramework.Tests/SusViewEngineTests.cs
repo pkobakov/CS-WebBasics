3eca240a73c2e39a9619f6f2c54ceae2a835bc56
Petar Kobakov <pkobakov@gmail.com>
2021-09-16 13:02:15 +0300

    Work on ViewEngine

---
+++ SUS/SUS/SUS.MvcFramework.Tests/SusViewEngineTests.cs
@@ -0,0 +1,42 @@
+﻿namespace SUS.MvcFramework.Tests
+{
+    using SUS.MvcFramework.ViewEngine;
+    using System;
+    using System.IO;
+    using Xunit;
+    public class SusViewEngineTests
+    {
+        [Theory]
+        [InlineData("CleanHtml")]
+        [InlineData("Foreach")]
+        [InlineData("IfElseFor")]
+        [InlineData("ViewModel")]
+        public void TestGetHtml(string fileName )
+        {
+
+            var viewModel = new TestViewModel
+            {
+                Name = "Golden Retriever",
+                Price = 12345.67M,
+                DateOfBirth = new DateTime(2019, 6, 23)
+
+
+            };
+
+            IViewEngine viewEngine = new SusViewEngine();
+            var view = File.ReadAllText($"ViewTests/{fileName}.html");
+            var result = viewEngine.GetHtml(view, viewModel);
+            var expectedResult = File.ReadAllText($"ViewTests/{fileName}.Result.html");
+            Assert.Equal(expectedResult, result);
+        }
+
+    }
+    public class TestViewModel
+    {
+
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public decimal Price { get; set; }
+    }
+         
+}
