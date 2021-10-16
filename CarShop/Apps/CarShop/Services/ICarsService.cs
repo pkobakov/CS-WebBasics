6286aced9e9c4e23a81ea29bcbc0ca6e6d36989a
Petar Kobakov <pkobakov@gmail.com>
2021-10-16 19:12:55 +0300

    CarShop Exam

---
+++ CarShop/Apps/CarShop/Services/ICarsService.cs
@@ -0,0 +1,11 @@
+﻿namespace CarShop.Services
+{
+    using CarShop.ViewModels.Cars;
+    using System.Collections.Generic;
+    public interface ICarsService
+    {
+        void CreateCar(string model,int year, string image, string plateNumber, string ownerId );
+        IEnumerable<CarViewModel> GetAll(string ownerId);
+
+    }
+}
---
+++ CarShop/Apps/CarShop/ViewModels/Issues/IssueViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarShop.ViewModels.Issues
+{
+   public class IssueViewModel
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string IsItFixed { get; set; }
+    }
+}
